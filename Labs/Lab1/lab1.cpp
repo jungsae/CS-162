7b6a7eb8f98a307f6c543a64c8c7c17a5c52ff52
jungsae <81615965+jungsae@users.noreply.github.com>
2021-04-05 21:59:01 +0900

    Add files via upload

--- Labs/Lab1/lab1.cpp
+++ Labs/Lab1/lab1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 using namespace std;
 
 struct mult_div_values
@@ -8,22 +9,11 @@ struct mult_div_values
     float div;
 };
 
-bool is_valid_dimension(char *n)
-{
-    if (n == 0)
-    {
-        "Please enter an integer greater than 0 for a matrix: ";
-        cin >> *n;
-    }
-
-    return &n;
-}
-
 mult_div_values **create_table(int n)
 {
     mult_div_values **table = new mult_div_values *[n];
 
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         table[i] = new mult_div_values[n];
     }
@@ -31,58 +21,100 @@ mult_div_values **create_table(int n)
     return table;
 }
 
+bool is_valid_dimension(char *n)
+{
+    while (*n < '1' || *n > '9')
+    {
+        cout << "You did not input a valid size." << endl;
+        cout << "Please enter an integer greater than 0 for a matrix: ";
+        cin >> n;
+    }
+}
+
 void set_mult_values(mult_div_values **table, int n)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
-            table[i][j].mult = i * j;
+            table[i][j].mult = (i + 1) * (j + 1);
         }
     };
 }
 
 void set_div_values(mult_div_values **table, int n)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 0; j < n; j++)
         {
-            table[i][j].div = (double)i / (double)j;
+            table[i][j].div = (float)(i + 1) / (j + 1);
         }
     }
 };
 
+void print_mult_div_table(mult_div_values **table, int n)
+{
+    cout << "Multiplication Table:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << table[i][j].mult << "\t";
+        }
+        cout << endl;
+    }
+
+    cout << "Division Table:" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << fixed;
+            cout.precision(2);
+            cout << table[i][j].div << "\t";
+        }
+        cout << endl;
+    }
+}
+
 void delete_table(mult_div_values **table, int n)
 {
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
     {
         delete[] table[i];
     }
     delete[] table;
 };
 
-int main(int argc, char **argv)
+void prompt_ask(int &y_n)
 {
-    if (argc != 2)
-        {
-            cout << "You did not input a valid size." << endl;
-            return 1;
-        }
-    int n = atoi(argv[1]);
-    int yOrN;        
-    mult_div_values **multDivTable = create_table(n);
-
-    do
+    int input;
+    cout << "Would you like to see a different size matrix (0-no, 1-yes)?" << endl;
+    cin >> input;
+    if (input == 1)
     {
-        set_mult_values(multDivTable,n);
-        set_div_values(multDivTable,n);
+        y_n = input;
+    }
+    else if (input == 0)
+    {
+        y_n = 0;
+    }
+}
 
-
-        cout << "Would you like to see a different size matrix (0-no, 1-yes)?" << endl;
-        cin >> yOrN;
-    } while (yOrN == 1);
-
+int main(int argc, char **argv)
+{   
+    int input;
+    do{
+    is_valid_dimension(argv[1]);
+    int n = atoi(argv[1]);
+    mult_div_values **multDivTable = create_table(n);
+    set_mult_values(multDivTable, n);
+    set_div_values(multDivTable, n);
+    print_mult_div_table(multDivTable, n);
     delete_table(multDivTable, n);
+    prompt_ask(input);
+    }while(input == 1);
+    
     return 0;
 }
